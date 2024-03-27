76b661aba164edf81f46d5f95f4bdc12de625f2e
pedrocasxz0110 <135608813+pedrocasxz0110@users.noreply.github.com>
2024-03-27 11:50:55 -0300

    Update Ex06.py

--- exercicio/Primeiro_Trimestre/Lista_Prova/Ex06.py
+++ exercicio/Primeiro_Trimestre/Lista_Prova/Ex06.py
@@ -1,8 +1,11 @@
+def soma(num1, num2, num3):
+
+    return num1 + num2 + num3
 
 numero1 = float(input("Digite o primeiro número: "))
 numero2 = float(input("Digite o segundo número: "))
 numero3 = float(input("Digite o terceiro número: "))
 
-soma = numero1 + numero2 + numero3
 
-print("A soma dos três números é:", soma)
+
+print("A soma dos três números é:", soma(numero1,numero2,numero3))
